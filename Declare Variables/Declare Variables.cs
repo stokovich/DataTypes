5a43a53f806c59b40a69c88d6c28a5d975e389cb
Stoyan Penchev <spenchev@gmail.com>
2017-07-28 11:52:59 +0300

    Add project files.

---
+++ Declare Variables/Declare Variables.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        uint num1 = 52130;
+        sbyte num2 = -115;
+        ulong num3 = 4825932;
+        byte num4 = 97;
+        short num5 = -10000;
+    }
+}
+
